d8187d8f6230a744102c86de166b25cd9f57fe1e
tomi <tomiwest@hotmail.com>
2023-04-24 10:58:57 +0300

    add stuling to gameboard info panel

--- client/src/components/GameBoardInfo.js
+++ client/src/components/GameBoardInfo.js
@@ -3,16 +3,33 @@ import React from 'react'
 const GameBoardInfo = ({ gameState }) => {
   const { id, inTurn, type, winner, isFinished } = gameState
   return (
-    <div className="md:self-start m-2 text-white">
-      <h2>Game ID {id}</h2>
-      <h2>Game type {type}</h2>
-      <h2>Game winner {winner}</h2>
+    <div className="md:self-start m-2 font-semibold divide-y-2 text-white border border-white p-2">
+      <h2>GAME ID: {id}</h2>
       <h2>
-        Is Finished:
-        {isFinished ? 'true' : 'false'}
+        GAME TYPE: <span className="text-amber-600 uppercase">{type}</span>
       </h2>
-      <h2 className="text-blue-400">inTurn {inTurn}</h2>
-      <h2 className="text-green-400">winner {winner}</h2>
+
+      <h2>
+        IS FINISHED:{' '}
+        <span
+          className={`${
+            isFinished ? 'text-green-600' : 'text-red-600'
+          } uppercase`}
+        >
+          {isFinished.toString()}
+        </span>
+      </h2>
+      <h2>
+        IN TURN: <span className="text-amber-600 font-extrabold">{inTurn}</span>
+      </h2>
+      {isFinished && (
+        <h2 className="text-green-500">
+          WINNER:{' '}
+          <span className={`text-amber-600`}>
+            {winner === null ? 'TIE' : winner}
+          </span>
+        </h2>
+      )}
     </div>
   )
 }
